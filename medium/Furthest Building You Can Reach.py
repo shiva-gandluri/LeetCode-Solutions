c4ee4794ec9ff832180c5c60c87670100e532d5b
shiva-gandluri <46939066+shiva-gandluri@users.noreply.github.com>
2022-10-10 01:02:09 -0700

    Adding Leetcode 1642

---
+++ medium/Furthest Building You Can Reach.py
@@ -0,0 +1,17 @@
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        if len(heights) == 1:
+            return len(heights)-1
+        
+        heap = []
+        for i in range(len(heights)-1):
+            diff = heights[i+1]-heights[i]
+            if diff <= 0:
+                continue
+            heappush(heap, diff)
+            if len(heap) > ladders:
+                bricks -= heappop(heap)
+            if bricks < 0: 
+                return i
+            
+        return len(heights)-1 
